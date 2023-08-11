1247ab33ba44a8cf6dc87c5d3d0d84757d1e71c3
KaiYanagisawa <12730ky@gmail.com>
2023-08-12 01:12:50 +0900

    num上限の追加

--- controller/handle_controller.go
+++ controller/handle_controller.go
@@ -9,7 +9,9 @@ import (
 
 func ShowFibNumber(c *gin.Context) {
 	num, _ := strconv.Atoi(c.Query("n"))
-	if num <= 0 {
+	
+	// NOTE: num=250000のとき実行時間が約200ms. それ以上のリクエストは受け付けないようにする
+	if num <= 0 || num > 250000{
 		BadRequest(c)
 		return
 	}
