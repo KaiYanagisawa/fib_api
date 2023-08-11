52514d46c71bd3efbd5cf1c4c8c53481e7e27194
KaiYanagisawa <12730ky@gmail.com>
2023-08-11 20:29:12 +0900

    フィボナッチ数計算処理

--- model/calc_fib.go
+++ model/calc_fib.go
@@ -1,6 +1,22 @@
 package model
 
-func CalcFib(num int) (fib_num int) {
-	fib_num = num
-	return  
+import "math/big"
+
+func CalcFib(num int) *big.Int{
+	fib_num := big.NewInt(0)
+	if num <= 2 {
+		fib_num.SetInt64(1)
+	} else {
+		pre_fib := big.NewInt(1)
+		cur_fib := big.NewInt(1)
+		tmp_fib := big.NewInt(0)
+
+		for i := 0; i < num-1; i++ {
+			tmp_fib.Add(pre_fib, cur_fib)
+			pre_fib.Set(cur_fib)
+			cur_fib.Set(tmp_fib)
+		}
+		fib_num.Set(pre_fib)
+	}
+	return fib_num
 }
