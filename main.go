84e84e6ac33ecd48fa00bbcce0eee70f1196ea44
KaiYanagisawa <12730ky@gmail.com>
2023-08-11 16:02:34 +0900

    ディレクトリ構成の変更

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"net/http"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	http.HandleFunc("fib/", handleControl)
-	http.ListenAndServe(":8080", nil)
+	router := gin.Default()	
+
+	router.Run(":8080")
 }
