d7b63e2fa8eedf6b44b9e149664dba9e3bf3bc9e
KaiYanagisawa <12730ky@gmail.com>
2023-08-13 00:50:56 +0900

    calc_fibテストコード実装

---
+++ model/calc_fib_test.go
@@ -0,0 +1,29 @@
+package model
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestCalcFib(t *testing.T) {
+	test := []struct {
+		num      int
+		expected string
+	}{
+		{0, "0"},
+		{1, "1"},
+		{2, "1"},
+		{5, "5"},
+		{10, "55"},
+		{99, "218922995834555169026"},
+	}
+
+	for _, tc := range test {
+		t.Run(fmt.Sprintf("num=%d", tc.num), func(t *testing.T) {
+			result := CalcFib(tc.num).String()
+			if result != tc.expected {
+				t.Errorf("CalcFib(%d) = %s; expected = %s", tc.num, result, tc.expected)
+			}
+		})
+	}
+}
